6551f77e99ddf98b432c1ac9c91cd823c54b670a
Richard Musime <richardmusime6@gmail.com>
2024-12-19 13:50:27 +0200

    added the tests to login , create a product, edit product and delete product

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -32,6 +32,7 @@ describe('Login Form Tests', () => {
         await mainContent.waitForDisplayed({ timeout: 10000 });
 
         await expect(mainContent).toBeExisting();
+        await browser.pause(3000)
     });
 });
 describe('Product Management', () => {
@@ -45,6 +46,7 @@ describe('Product Management', () => {
 
         const createDialog = await $('.createProduct');
         await createDialog.waitForDisplayed({ timeout: 5000 });
+        await browser.pause(3000)
 
         const testProduct: TestProduct = {
             id: '1',
@@ -65,29 +67,53 @@ describe('Product Management', () => {
         await okButton.waitForDisplayed({timeout: 20000})
         await okButton.click();
 
+        await browser.pause(5000)
+
 
 
         const productRow = await $(`//*[contains(@class, "idTable")]//*[contains(@class, "id") and text()="${testProduct.id}"]/ancestor::tr`);
         await productRow.waitForDisplayed({ timeout: 10000 });
         
         const editButton = await productRow.$('button=Edit');
-        await editButton.click();
+        await editButton.click();        
+        await browser.pause(3000)
 
         const updatedName = `Updated ${testProduct.name}`;
-        await $('#productNameInput').setValue(updatedName);
-        await $('button=Save').click();
-        await browser.pause(1000);
+        await $('input[name="Name"]').setValue(updatedName);
+        const updatedDescription = `Updated ${testProduct.description}`;
+        await $('input[name="Description"]').setValue(updatedDescription);
+        const updatedPrice = `2${testProduct.price}`;
+        await $('input[name="Price"]').setValue(updatedPrice);
+        await browser.pause(5000)
+        await $('button=Create').click();
 
-        const updatedRow = await $(`//*[contains(text(),'${updatedName}')]`);
-        const isUpdated = await updatedRow.isExisting();
-        expect(isUpdated).toBe(true);
 
-        const deleteButton = await $(`//*[contains(text(),'${updatedName}')]/ancestor::tr//button[contains(text(),'Delete')]`);
-        await deleteButton.click();
-        await browser.pause(1000);
+        const okBu = await $('button=OK');
+        await okBu.waitForDisplayed({timeout: 20000})
+        await okBu.click();
+        await browser.pause(5000)
 
-        const isDeleted = await updatedRow.isExisting();
-        expect(isDeleted).toBe(false);
+        const productsRow = await $(`//*[contains(@class, "idTable")]//*[contains(@class, "id") and text()="${testProduct.id}"]/ancestor::tr`);
+        await productsRow.waitForDisplayed({timeout: 5000})
+
+        await browser.pause(5000)
+
+        const deleteButton = await productsRow.$('button=Delete')
+        await deleteButton.click()
+        await browser.pause(5000)
+
+        const confirmationButton = await $('button=Yes')
+        await confirmationButton.click()
+        await browser.pause(1000)
+
+        const ok = await $('button=OK');
+        await ok.waitForDisplayed({timeout: 20000})
+        await ok.click();
+        await browser.pause(5000)
+
+
+
+
     });
 });
 
